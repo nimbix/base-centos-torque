3c7b0680dc78f5ca8b6ac8561af546aa71808aca
Leo Reiter <leo.reiter@nimbix.net>
2017-08-30 16:56:32 -0500

    fix syntax error

--- scripts/torque/launch.sh
+++ scripts/torque/launch.sh
@@ -51,7 +51,7 @@ fi
 echo "$MASTER" | tee $SPOOL/server_name
 echo "\$pbsserver $MASTER" | tee $SPOOL/mom_priv/config
 
-if [ -d /etc/munge && ! -f /etc/munge/munge.key ]; then
+if [ -d /etc/munge -a ! -f /etc/munge/munge.key ]; then
     dd if=/home/nimbix/.ssh/id_rsa of=/etc/munge/munge.key bs=1024 count=1
     chown munge /etc/munge/munge.key
     chmod 400 /etc/munge/munge.key
