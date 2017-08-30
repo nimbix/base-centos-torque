de2da61fca51039310dbb909f8d46c5b07bcf676
Leo Reiter <leo.reiter@nimbix.net>
2017-08-30 15:23:16 -0500

    refactor and offer alternative builds

---
+++ build_torque.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+yum install -y boost-devel \
+    libxml2-devel \
+    make openssl-devel \
+    rpm-build \
+    git \
+    vixie-cron
+yum groupinstall -y 'Development Tools'
+yum clean all
+cd /tmp
+git clone -b 6.0.2 https://github.com/adaptivecomputing/torque.git
+cd /tmp/torque
+./autogen.sh
+./configure
+make rpm
+cp -r /root/rpmbuild/RPMS/$(uname -m) /tmp/PKG
+rm -rf /root/rpmbuild/RPMS/$(uname -m)
+cd /tmp/PKG
+rpm -ivh *.rpm
+rm -rf *.rpm
+cd /tmp
+rm -rf torque
+
