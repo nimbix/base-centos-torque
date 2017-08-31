3062696261c2ce9fdd44c47e88afe7303c3a036d
Leo Reiter <leo.reiter@nimbix.net>
2017-08-31 09:28:46 -0500

    better pbs_server setup

--- scripts/torque/install7.sh
+++ scripts/torque/install7.sh
@@ -7,6 +7,8 @@ grep -q /usr/local/scripts/torque/launch.sh /etc/rc.local || \
     >>/etc/rc.local
 chmod +x /etc/rc.d/rc.local
 
+pbs_server -f -t create
+
 echo "Launch TORQUE on all nodes: /usr/local/scripts/torque/launch_all.sh"
 echo "OR"
 echo "Launch TORQUE nodes individually: /usr/local/scripts/torque/launch.sh"
