090c970fbf030ae87dfda74b05647dd9d6ee7dfa
Leo Reiter <leo.reiter@nimbix.net>
2017-08-31 09:40:55 -0500

    install torque-mom and also do not enable rc.local so we can debug

--- scripts/torque/install7.sh
+++ scripts/torque/install7.sh
@@ -5,7 +5,7 @@ echo "Assumes CentOS 7 TORQUE packages!!!"
 grep -q /usr/local/scripts/torque/launch.sh /etc/rc.local || \
     echo "su -l -c 'sudo /usr/local/scripts/torque/launch.sh' nimbix" \
     >>/etc/rc.local
-chmod +x /etc/rc.d/rc.local
+#chmod +x /etc/rc.d/rc.local
 
 pbs_server -f -t create
 
